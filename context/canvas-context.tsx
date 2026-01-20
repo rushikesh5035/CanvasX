2bf44919c128d404fb926e71cd11461c191ebe1e
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-01-21 02:06:20 +0530

    feat: create CanvasContext for managing frames, themes, and loading states

---
+++ context/canvas-context.tsx
@@ -0,0 +1,108 @@
+import { THEME_LIST, ThemeType } from "@/lib/themes";
+import { FrameType } from "@/types/project";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export type LoadingStatusType =
+  | "idle"
+  | "running"
+  | "analyzing"
+  | "generating"
+  | "completed";
+
+export interface CanvasContextType {
+  theme?: ThemeType;
+  setTheme: (id: string) => void;
+  themes: ThemeType[];
+
+  frames: FrameType[];
+  setFrames: (frames: FrameType[]) => void;
+  updateFrame: (id: string, data: Partial<FrameType>) => void;
+  addFrame: (frame: FrameType) => void;
+
+  selectedFrameId: string | null;
+  selectedFrame: FrameType | null;
+  setSelectedFrameId: (id: string | null) => void;
+
+  loadingStatus: LoadingStatusType;
+}
+
+const CanvasContext = createContext<CanvasContextType | undefined>(undefined);
+
+export const CanvasProvider = ({
+  children,
+  initialFrames,
+  initialThemeId,
+  hasInitialData,
+  projectId,
+}: {
+  children: React.ReactNode;
+  initialFrames: FrameType[];
+  initialThemeId?: string;
+  hasInitialData: boolean;
+  projectId: string | null;
+}) => {
+  const [themeId, setThemeId] = useState<string>(
+    initialThemeId || THEME_LIST[0].id,
+  );
+
+  const [frames, setFrames] = useState<FrameType[]>(initialFrames);
+
+  const [selectedFrameId, setSelectedFrameId] = useState<string | null>(null);
+
+  const [loadingStatus, setLoadingStatus] = useState<LoadingStatusType>(
+    hasInitialData ? "idle" : "running",
+  );
+
+  const theme = THEME_LIST.find((theme) => theme.id === themeId);
+
+  const selectedFrame =
+    selectedFrameId && frames.length != 0
+      ? frames.find((frame) => frame.id === selectedFrameId) || null
+      : null;
+
+  // update the loading state inngest realtime event
+
+  const addFrame = useCallback((frame: FrameType) => {
+    setFrames((prev) => [...prev, frame]);
+  }, []);
+
+  const updateFrame = useCallback((id: string, data: Partial<FrameType>) => {
+    setFrames((prev) => {
+      return prev.map((frame) =>
+        frame.id === id ? { ...frame, ...data } : frame,
+      );
+    });
+  }, []);
+
+  return (
+    <CanvasContext.Provider
+      value={{
+        theme,
+        setTheme: setThemeId,
+        themes: THEME_LIST,
+        frames,
+        setFrames,
+        selectedFrameId,
+        selectedFrame,
+        setSelectedFrameId,
+        updateFrame,
+        addFrame,
+        loadingStatus,
+      }}
+    >
+      {children}
+    </CanvasContext.Provider>
+  );
+};
+
+export const useCanvas = () => {
+  const ctx = useContext(CanvasContext);
+  if (!ctx) throw new Error("useCanvas must be used inside CanvasProvider");
+  return ctx;
+};
