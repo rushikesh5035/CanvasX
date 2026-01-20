df89cf67678c1362dc7d26a900405b53d69618b5
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-01-20 23:28:14 +0530

    feat: implement AI-powered project name generation server action and add project GET/POST API endpoints and also add GET endpoint for fetching individual project by id

---
+++ app/api/project/route.ts
@@ -0,0 +1,71 @@
+import { generateProjectName } from "@/app/action/action";
+import prisma from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { prompt } = await request.json();
+    const session = await getKindeServerSession();
+    const user = await session?.getUser();
+
+    if (!user) throw new Error("Unauthorized");
+
+    if (!prompt || typeof prompt !== "string")
+      throw new Error("Invalid prompt");
+
+    const userId = user.id;
+
+    const projectName = await generateProjectName(prompt);
+
+    const project = await prisma.project.create({
+      data: {
+        userId,
+        name: projectName,
+      },
+    });
+
+    // Trigger the inngest function to create project
+
+    return NextResponse.json({
+      success: true,
+      data: project,
+    });
+  } catch (error) {
+    console.log("Error occured", error);
+    return NextResponse.json(
+      {
+        error: "Failed to create project",
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const session = await getKindeServerSession();
+    const user = await session?.getUser();
+
+    if (!user) throw new Error("Unauthorized");
+
+    const project = await prisma.project.findMany({
+      where: { userId: user.id },
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json({
+      success: true,
+      data: project,
+    });
+  } catch (error) {
+    console.log("Error occured", error);
+    return NextResponse.json(
+      {
+        error: "Failed to fetch projects",
+      },
+      { status: 500 },
+    );
+  }
+}
