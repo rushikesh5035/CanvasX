adeae8fe445b2f53983db46f315f683e9783a9c3
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-01-21 14:35:57 +0530

    feat: create floating toolbar with AI prompt, theme picker, and save actions

---
+++ components/canvas/canvas-floating-toolbar.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { useCanvas } from "@/context/canvas-context";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { CameraIcon, ChevronDown, Palette, Save, Wand2 } from "lucide-react";
+import AIPromptInput from "../common/prompt-input";
+import { useState } from "react";
+import { parseThemeColors } from "@/lib/themes";
+import { cn } from "@/lib/utils";
+import ThemeSelector from "./theme-selector";
+import { Separator } from "../ui/separator";
+
+const CanvasFloatingToolbar = () => {
+  const { themes, theme: currentTheme, setTheme } = useCanvas();
+
+  const [promptText, setPromptText] = useState<string>("");
+
+  return (
+    <div className="fixed top-6 left-1/2 -translate-x-1/2">
+      <div className="w-full max-w-2xl bg-background dark:bg-gray-950 rounded-full shadow-xl border">
+        <div className="flex flex-row items-center gap-2 px-3">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                size="icon-sm"
+                className="px-4  bg-linear-to-r
+                 from-purple-500 to-indigo-600
+                  text-white rounded-2xl
+                  shadow-lg shadow-purple-200/50 cursor-pointer"
+              >
+                <Wand2 className="size-4" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              className="w-80 p-2!
+             rounded-xl! shadow-lg border mt-1
+            "
+            >
+              <AIPromptInput
+                promptText={promptText}
+                setPromptText={setPromptText}
+                className="min-h-[150px] ring-1! ring-purple-500!
+                rounded-xl! shadow-none border-muted
+                "
+                hideSubmitBtn={true}
+              />
+              <Button
+                className="mt-2 w-full
+                  bg-linear-to-r
+                 from-purple-500 to-indigo-600
+                  text-white rounded-2xl
+                  shadow-lg shadow-purple-200/50 cursor-pointer
+                "
+              >
+                Design
+              </Button>
+            </PopoverContent>
+          </Popover>
+
+          <Popover>
+            <PopoverTrigger>
+              <div className="flex items-center gap-2 px-3 py-2">
+                <Palette className="size-4" />
+                <div className="flex gap-1.5">
+                  {themes?.slice(0, 4)?.map((theme, idx) => {
+                    const color = parseThemeColors(theme.style);
+                    return (
+                      <div
+                        role="button"
+                        key={idx}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setTheme(theme.id);
+                        }}
+                        className={cn(
+                          `w-6.5 h-6.5 rounded-full cursor-pointer
+                           `,
+                          currentTheme?.id === theme.id &&
+                            "ring-1 ring-offset-1",
+                        )}
+                        style={{
+                          background: `linear-gradient(135deg, ${color.primary}, ${color.accent})`,
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+
+                <div className="flex items-center gap-1 text-sm">
+                  +{themes?.length - 4} more
+                  <ChevronDown className="size-4" />
+                </div>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent
+              className="px-0 rounded-xl
+            shadow border"
+            >
+              <ThemeSelector />
+            </PopoverContent>
+          </Popover>
+
+          {/* Divider */}
+          <Separator orientation="vertical" className="h-4!" />
+
+          <div className="flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="icon-sm"
+              className="rounded-full cursor-pointer"
+            >
+              <CameraIcon className="size-4.5" />
+            </Button>
+            <Button
+              variant="default"
+              size="sm"
+              className="rounded-full cursor-pointer"
+            >
+              <Save className="size-4" />
+              Save
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasFloatingToolbar;
