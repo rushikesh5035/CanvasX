fd8f917f10356777ad6826a47f196821a527f0c4
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-01-21 17:24:37 +0530

    refactor: Simplify project page by removing unused frame/theme variables and fix spacing

--- app/(routes)/project/[id]/page.tsx
+++ app/(routes)/project/[id]/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import Canvas from "@/components/canvas";
 import ProjectHeader from "@/components/project/project-header";
+import { CanvasProvider } from "@/context/canvas-context";
 import { useGetProjectById } from "@/features/use-project-id";
 import { useParams } from "next/navigation";
 
 const Page = () => {
   const params = useParams();
-
   const id = params.id as string;
 
   const { data: project, isPending } = useGetProjectById(id);
 
-  const frames = project?.frames || [];
-  const theme = project?.theme || "";
+  // const frames = project?.frames || [];
+  // const themeId = project?.theme || "";
+
+  const hasInitialData = project?.frames.length > 0;
 
   if (!isPending && !project) {
     return <div>Project not found</div>;
@@ -21,6 +24,23 @@ const Page = () => {
   return (
     <div className="relative h-screen w-full flex flex-col">
       <ProjectHeader projectName={project?.name} />
+
+      <CanvasProvider
+        initialFrames={project?.frames}
+        initialThemeId={project?.theme}
+        hasInitialData={hasInitialData}
+        projectId={project?.id}
+      >
+        <div className="flex flex-1 overflow-hidden">
+          <div className="relative flex-1">
+            <Canvas
+              projectId={project?.id}
+              projectName={project?.name}
+              isPending={isPending}
+            />
+          </div>
+        </div>
+      </CanvasProvider>
     </div>
   );
 };
