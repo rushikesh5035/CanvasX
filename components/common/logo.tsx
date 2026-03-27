08b2260288078844c8f1413ed2d5e452a1cf1c43
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-03-28 00:26:20 +0530

    fix: center logo by removing flex-1 and use card-foreground color

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -1,15 +1,11 @@
-import React from "react";
-
 import Image from "next/image";
-import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex flex-1 items-center gap-1 text-2xl">
-      <span className="text-foreground font-semibold">Canvas</span>
-      {/* <span className="inline-block font-extrabold text-primary">X</span> */}
+    <div className="flex items-center gap-1 text-2xl">
+      <span className="text-card-foreground font-semibold">Canvas</span>
       <Image src={"/assets/logo.png"} alt="logo" width={25} height={25} />
-    </Link>
+    </div>
   );
 };
 
